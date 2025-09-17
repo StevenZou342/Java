dc3077ca7bdaeac53c556357fc3322ed98bb7248
StevenZou342 <stevenz79@nycstudents.net>
2025-09-17 16:33:13 +0000

    hi

---
+++ Java-4-Activity/Main.java
@@ -0,0 +1,61 @@
+class Main {
+	public static void main(String[] args) {
+    	(new Main()).init();
+	}
+
+ void init(){
+// Define a variable for each situation in the challenges below. Assign a value to each variable.
+// Display the value of each variable to the console.
+
+/*  Challenge 1:    
+    1) student's age 
+    2) number of people that live with you
+    3) number of items in a cart 
+*/
+   int age = 15;
+   System.out.println(age);
+   System.out.println("Student age is  " +  age);
+   int member = 5;
+   int charts = 15;
+   System.out.println(member + " people live with me. ");
+    System.out.println(charts + " item in the cart ");
+/*  Challenge 2:
+    1) temperature 
+    2) student's GPA
+    3) growth rate of your investment
+*/
+    double temp= 100.5;  
+     System.out.println("Temperature is  " +  temp + " degrees");
+    double gpa= 3.9; 
+    System.out.println("Gpa is " + gpa);
+    double growth = 14.5;
+    System.out.println("Growth is  " +  growth + " % per year");
+/*  Challenge 3:
+    1) Student's last name 
+    2) student's first initial
+    3) description of an item 
+*/
+    String name= "Smith";
+    System.out.println("Last name is " + name);
+    String intial= "JS";
+    System.out.println("Intial is " + intial);
+    String des= "Soild and brittle";
+    System.out.println("This item is " + des);
+/*  Challenge 4:
+    1) the status whether lights are on
+    2) the status whether the game is over
+    3) the status whether the battery is charging
+*/
+    boolean lights = false;
+    System.out.println("The light is on true or false? " + lights);
+    boolean game = true;
+     System.out.println("The game over true or false? " + game);
+    boolean battery = true;
+     System.out.println("The battery charging true or false? " + battery);
+
+
+    // **************************************************
+    // **** Don't write any code below here.  ***********
+    // **************************************************
+  }
+}
