974c41f5c81787c95944bb0729081c2ffdaf9b06
StevenZou342 <stevenz79@nycstudents.net>
2025-10-22 16:32:15 +0000

    j

---
+++ Java-12-Activity/Main.java
@@ -0,0 +1,68 @@
+class Main {
+
+  public static void main(String[] args) {
+    (new Main()).init();
+  }
+  void print(Object o){ System.out.println(o);}
+  void printt(Object o){ System.out.print(o);}
+
+  void init(){
+   System.out.println(creditCardType("34"));
+  }
+/*
+    Challenge 1:
+    Write a function creditCardType() that accepts a credit card number (as a string) and returns the type of credit card based on the starting numbers (see below)
+    Starts with:
+    4 - Visa
+    34 or 37 - American Express
+    36 - Diner's Club
+    51 or 55 - Mastercard
+    6011 or 65 - Discover
+    */
+    String creditCardType(String num){
+      if(num.substring(0,1).equals(num.indexOf("4"))){
+        return "visa";
+      }else if(num.substring(0,2).equals(num.indexOf("34")) || (num.substring(0,2).equals(num.indexOf("37")))){
+        return "American Express";
+      }else if(num.substring(0,2).equals(num.indexOf("36"))){
+        return "Mastercard";
+      }else if(num.substring(0,2).equals(num.indexOf("51")) ||  (num.substring(0,2).equals(num.indexOf("5")))){
+        return "American Express";
+      }else{
+        return "Discover";
+      }
+    }
+    /*
+    Challenge 2:
+    Write a function pigLatin() which accepts a word and returns a new word made up by switching the fist letter with the rest of the word and adding "ay" after. Example: "pig"==> "igpay"
+    */
+      
+
+    /*
+    Challenge 3:
+    Write a function nycLocate() that accepts a zip code (as a string) and returns the appropriate NYC borough determined by the fist three numbers of the zip code.
+    Starting numbers of zip code:
+    Manhattan :100, 101, 102
+    Staten Island : 103
+    Bronx : 104
+    Brooklyn : 112
+    Queens : 113, 114, 111
+    */
+
+  
+
+    /*
+    Challenge 4:
+    Create a function getMonth() that accepts a date in the format MM/DD/YYYY and returns the month: Example 07/21/2023 ==> "July"
+  */
+
+  
+  
+    /*
+    Challenge 5:
+    Create a function validatePswd() that accepts a password. The password must have 5 to 8 characters that are letters, numbers or any symbol except ^, *, (, )
+    Return true if it's a valid password; otherwise return false.
+    */
+
+  
+}
--- Java-12-CW/Main.java
+++ Java-12-CW/Main.java
@@ -3,28 +3,38 @@ class Main {
   public static void main(String[] args) {
     (new Main()).init();
   }
-  void print(Object o){ System.out.println(o);}
-  void printt(Object o){ System.out.print(o);}
 
   void init(){
-   
-
+  // college credit 
+  System.out.println(collegeCredit("MKUFCE1"));
+  System.out.println(collegeCredit("MKUFC1"));
+  // counselor
+  System.out.println(getCounselor("1D0"));
+  System.out.println(getCounselor("D10"));
+  System.out.println(getCounselor("1F0"));
+  System.out.println(getCounselor("1C0"));
+  System.out.println(getCounselor("1G0"));
+  System.out.println(getCounselor("X10"));
+  // image type
+  System.out.println(imageType("firstQuarter.png"));
+  System.out.println(imageType("documentX.pdf"));
+  System.out.println(imageType("pic1.JPEG"));
+  System.out.println(imageType("pic2.jpg"));
+  System.out.println(imageType("ironman.gif"));
     
   }
 /*
 Problem 1:
 Write a function collegeCredit() that accepts a course code and if a course gets college credit return true otherwise return false. If the 6th letter of the course code is "E" or "M" then it is a college credit course.
 */
-boolean collegeCredit(String name){
-  if(name.substring(6,7).equals("E"||"M") ){
-    return true;
-  }else{
-    return false;
+  boolean collegeCredit(String course){
+    if(course.substring(5,6).equals("M") ||
+       course.substring(5,6).equals("E") )
+      return true;
+    else
+      return false;
   }
-  }
-}
-
-
+  
 /*
 Problem 2:
 Write a function getCounselor() that accepts a student's official class code and returns their guidance counselor based on the either the first or the middle character of the official class code being:
@@ -35,21 +45,22 @@ E-Eyzengart
 F-Flores
 G-Gibson
 */
-String getConselour(String code){
-  if(code.substring(0,1)||code.substring(1,2).equals(code.indexOf("B"))){
-    return "Berrouet";
-    }else if(code.substring(0,1)||code.substring(1,2).equals(code.indexOf("C"){
-       return "Chu";
-    }else if(code.substring(0,1)||code.substring(1,2).equals(code.indexOf("D"){
-       return "Dinn";
-   }else if(code.substring(0,1)||code.substring(1,2).equals(code.indexOf("F"){
-    return "Flores";
-    else if(code.substring(0,1)||code.substring(1,2).equals(code.indexOf("E"){
-        reuturn "Eyzengart";
-    }else{
-      return "Gibson";
+  String getCounselor(String ofcl){
+    if(ofcl.substring(0,1).equals("D") || ofcl.substring(1,2).equals("D")){
+      return  "Dinn";
     }
-}
+    else if(ofcl.substring(0,1).equals("F") || ofcl.substring(1,2).equals("F")){
+      return  "Flores";
+    }
+    else if(ofcl.substring(0,1).equals("C") || ofcl.substring(1,2).equals("C")){
+      return  "Chu";
+    }
+    else if(ofcl.substring(0,1).equals("G") || ofcl.substring(1,2).equals("G")){
+      return  "Gibson";
+    }
+    else
+      return "GC unknown";
+  }
 
 /*  
 Problem 3:
@@ -59,8 +70,24 @@ JPEG or JPG - Joint Photographic Experts Group
 PNG - Portable Network Graphics
 GIF - Graphics Interchange Format
 PDF - Portable Document Format(not an image type)
-*/ 
-
- 
+*/  
+  String imageType(String fileName){
+    int pos = fileName.indexOf(".");
+    
+    if (fileName.substring(pos+1).equalsIgnoreCase("JPEG") || fileName.substring(pos+1).equalsIgnoreCase("JPG")){
+        return "Joint Photograpic Experts Group";
+       }
+    else if (fileName.substring(pos+1).equalsIgnoreCase("PNG") ){
+        return "Portable Network Graphics";
+       }
+    else if (fileName.substring(pos+1).equalsIgnoreCase("GIF") ){
+        return "Graphics Interchage Format";
+       }
+    else if (fileName.substring(pos+1).equalsIgnoreCase("PDF") ){
+        return "Portable Document Format(not an image type)";
+       }
+    else 
+      return "Unknow file type";
+  }
   
 }
